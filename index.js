2d0862e23a1c2be09494958b2cd3de80dba742d9
Thony Rasolofondraibe <thonyrasolofondraibe1883@gmail.com>
2025-03-26 19:31:52 +0300

    feat (initialisation) :  initalisation du projet
    création du projet , structure du projet

---
+++ index.js
@@ -0,0 +1,13 @@
+// Import du module HTTP intégré à Node
+const http = require('http');
+
+// Création du serveur
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello Node.js!\n');
+});
+
+// Écoute sur le port 3000
+server.listen(3000, () => {
+  console.log('Serveur en écoute sur http://localhost:3000/');
+});
